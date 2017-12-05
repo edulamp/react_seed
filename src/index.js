a8e019940ead72a4ffdf41fb1c8a0c932020d09a
flamingwhite <ever0702@gmail.com>
2017-12-05 16:17:26 -0500

    3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import * as R from 'ramda';
+import { curry } from 'ramda';
 
-console.log(R);
+export { curry };
+const add = (...params) => params;
+
+add(
+  1,
+  2,
+  3,
+  4,
+  5,
+  6.9999999999999999999999993333333,
+  7.888888888888888888888888888888333,
+  3.44444444444444,
+  5.444444444444,
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,37 @@
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
 
 const DIST_DIR = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  devtool: 'inline-source-map',
   entry: [path.resolve(__dirname, 'src/index')],
+  devtool: 'inline-source-map',
   target: 'web',
   output: {
     path: DIST_DIR,
-    filename: 'dist/bundle.js'
+    filename: 'dist/bundle.js',
   },
   devServer: {
     inline: true,
-    port: 5000
+    port: 5000,
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'src'),
-        loaders: ['babel-loader']
+        loaders: ['babel-loader'],
       },
       { test: /(\.css)$/, loaders: ['style-loader', 'css-loader'] },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
       { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-    ]
-  }
+        loader: 'url?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  },
 };
