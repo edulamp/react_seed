bdf815ed248e89909853bea8581f09f4a0a6e1c7
flamingwhite <ever0702@gmail.com>
2017-12-13 10:48:04 -0500

    d

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import { curry } from 'ramda';
-import 'rxjs';
 import './app.css';
 
 console.log('app load');
 
+const x = [1, 2];
+console.log([...x, 3, 4, 5]);
+
 export function sayHello() {
 	return 'hello world';
 }
-
-export { curry };
